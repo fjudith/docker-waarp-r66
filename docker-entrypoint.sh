b3895910a86310d09f213af45a86bf1e51a88e62
Florian JUDITH <florian.judith.b@gmail.com>
2017-04-13 10:58:45 +0200

    Fixed typo

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -67,9 +67,9 @@ export WAARP_SNMP_PRIVPASS=${WAARP_SNMP_PRIVPASS:-"password"}
 echo $(date --rfc-3339=seconds) 'Deploying XML configuration files if required'
 
 if [ ! -f ${SERVER_CONFIG} ]; then
-	mkdir -p "/etc/waarp/conf.d/${WAARP_APPNAME}"
+    mkdir -p "/etc/waarp/conf.d/${WAARP_APPNAME}"
     mkdir -p "/etc/waarp/certs"
-	cp -vn /tmp/conf.d/template/*.xml /etc/waarp/conf.d/${WAARP_APPNAME}/
+    cp -vn /tmp/conf.d/template/*.xml /etc/waarp/conf.d/${WAARP_APPNAME}/
 fi
 
 
